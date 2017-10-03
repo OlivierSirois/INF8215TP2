5d2529a379eaf3b486cdba06d89469ca636fbba7
IngridNavarroA <ingrid.navarro.an@gmail.com>
2017-10-03 15:23:07 -0400

    max2, max3

--- ex4_3.pl
+++ ex4_3.pl
@@ -1,6 +1,26 @@
 
-% R = X + Y
+% 1. R = X + Y
 sum(X, Y, R) :- R is Y+X.
 
-% M = max(X, Y)
-max2(X, Y, M) :- M is X>Y.
+% 2. M = max(X, Y)
+
+% max(X, Y) :- X>Y.
+% max2(X, Y, M) :- M is max(X, Y, M).
+% max3(X, Y, Z, M) :- M is max(max(X, Y), Z). 
+
+max2(X,X,X).
+max2(X,Y,X) :- X>Y.
+max2(X,Y,Y) :- Y>X.
+
+max3(X,X,X,X).
+max3(X,Y,Z,X) :- X>=Y,X>=Z.
+max3(X,Y,Z,Y) :- Y>=X,Y>=Z.
+max3(X,Y,Z,Z) :- Z>=X,Z>=Y.
+
+% 3. d(F, X, G)
+red(C*X^E, R) :- R is E-1.
+coef(C*X^E, R) :- R is C*E. 
+d(F, X, G) :- G is coef(F, R)*X^red(F, R)
+
+
+ 
