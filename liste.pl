a3582881da4307c2e3b29b82d33bfb4c374b3b0a
Imanne El maachi <30446428+imanneelmaachi@users.noreply.github.com>
2017-11-05 00:53:33 -0400

    bug fixed
    il reste le enumerate que je dois ajuster un peu

--- liste.pl
+++ liste.pl
@@ -1,9 +1,9 @@
-max([H1, H2 |T],M):- max([H|T],M), max(H1,H2,H).
+max([H1, H2 |T],M):-  maxi(H1,H2,H), max([H|T],M).
 max([H1],H1).
 
-max(X,X,X).
-max(X,Y,X) :- X>Y.
-max(X,Y,Y) :- Y>X.
+maxi(X,X,X).
+maxi(X,Y,X):- X>Y.
+maxi(X,Y,Y) :- Y>X.
 
 
 %somme d'une vecteur
@@ -11,24 +11,28 @@ sumvecteur([],0).
 sumvecteur([A|T], S) :- sumvecteur(T, S1), S is S1+A.
 
 % retourne valeur vecteur
-nth(N[H1|T],R):- R ==H.
-nth(N,[H1|T],R):- N1 is N-1 ,nth(N1,T,R),R/==H1.
+nth(N,[H1|_],R):- N==1 , R = H1.
+nth(N,[_|T],R):- N >1, N1 is N-1 ,nth(N1,T,R).
 
 
 zip([],[],[]). 
-zip([H1|T1],[H2|T2],R):- zip(T1,T2,R), R=[H1,H2].
+zip([H1|T1],[H2|T2] ,[[H1,H2]|T]):- zip(T1,T2,T).
+
 
 enumerate(0,[]). 
-enumerate(N,L):- N1 is N-1, enumerate(N1,L), L[N1|_]
+enumerate(N,[S|N]):- N > 0, N1 is N-1, enumerate(N1,S).
 
-pieces =[2,1,0.25,0.1,0.05]
-rendmonnaie(Argent,Argent).
-rendmonnaie(Argent, Prix) :- M = Argent-Prix, RM(M,[2,1,0.25,0.1,0.05]).
 
-RM(M,[H1|T]):- H1> M, RM(M,T).
-RM(M,[H1|T]):- M>H1, n= floor(M/H1), M1=(M-n*H1),RM(M1,T), write(n),write('pieces de '), write(H1) , nl.
-RM(M,[]). 
+rendmonnaie(Argent, Argent).
+rendmonnaie(Argent, Prix) :- M is Argent - Prix, rm(M,[2,1,0.25,0.1,0.05]).
 
+
+%rm(M,[H1|T]):- H1> M, rm(M,T).
+rm(M,[H1|T]):-  D is M/H1, N is floor(D),  M1 is M-N*H1 ,rm(M1,T), write(N),write(' pieces de '), write(H1) , nl.
+rm(_,[]). 
+
+
+ffloor(M,N,R):- R is M-N.
 
 
 
