cc3514d4108cdd91f70e0149b076e1f8b42a1670
Imanne El maachi <30446428+imanneelmaachi@users.noreply.github.com>
2017-11-05 21:36:56 -0500

    updated enumerate function

--- liste.pl
+++ liste.pl
@@ -3,7 +3,7 @@ max([H1],H1).
 
 maxi(X,X,X).
 maxi(X,Y,X):- X>Y.
-maxi(X,Y,Y) :- Y>X.
+maxi(X,Y,Y):- Y>X.
 
 
 %somme d'une vecteur
@@ -18,21 +18,20 @@ nth(N,[_|T],R):- N >1, N1 is N-1 ,nth(N1,T,R).
 zip([],[],[]). 
 zip([H1|T1],[H2|T2] ,[[H1,H2]|T]):- zip(T1,T2,T).
 
+enumerate(N,S):- N1 is N-1, range(0,N1,S).
+range(S,S,[S]).
+range(S,N1,[S|L]) :- S < N1, S1 is S + 1, range(S1,N1,L).
 
-enumerate(0,[]). 
-enumerate(N,[S|N]):- N > 0, N1 is N-1, enumerate(N1,S).
+
 
 
 rendmonnaie(Argent, Argent).
 rendmonnaie(Argent, Prix) :- M is Argent - Prix, rm(M,[2,1,0.25,0.1,0.05]).
 
 
-%rm(M,[H1|T]):- H1> M, rm(M,T).
 rm(M,[H1|T]):-  D is M/H1, N is floor(D),  M1 is M-N*H1 ,rm(M1,T), write(N),write(' pieces de '), write(H1) , nl.
 rm(_,[]). 
 
-
-ffloor(M,N,R):- R is M-N.
 
 
 
